8547713509ebe90545250ecd753c93ee0dde0a7c
SamBaker <qasambaker@yandex.ru>
2024-12-10 21:50:00 +0500

    commit Telecom Tests

---
+++ src/test/java/FirstTelecomWebTests.java
@@ -0,0 +1,83 @@
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
+
+@Tag("TELECOM")
+@DisplayName("Тестирование сайта Telecom")
+public class FirstTelecomWebTests extends TestBase {
+
+    @DisplayName("Открытие информации по увеличению скорости интернета")
+    @Test
+    void GetInformationBoostInternetSpeedTest() {
+        step("Open main page", () -> {
+            open("/");
+            $("header").should(appear);
+            executeJavaScript("let footer = document.querySelector('footer'); " +
+                    "if (footer) footer.remove();");
+        });
+
+        step("Navigate to Internet section", () -> {
+            $("a.main-menu__navigation-link[href='/ru/common/internet']").shouldBe(visible).click();
+            $("div.TabPackageService").should(appear);
+        });
+
+        step("Open Sapa+ details page", () -> {
+            $("div.SAPAPLUSBanner").shouldBe(visible);
+            $("div.SAPAPLUSBanner button.check-btn").shouldHave(text("Подробнее")).click();
+        });
+    }
+
+    @DisplayName("Получение информации по подключению Тарифа Keremet TV")
+    @Test
+    void GetInformationActivateServicesTest() {
+        step("Open main page", () -> {
+            open("/");
+            $("header").should(appear);
+            executeJavaScript("let footer = document.querySelector('footer'); " +
+                    "if (footer) footer.remove();");
+        });
+
+        step("Navigate to Internet section", () -> {
+            $("a.main-menu__navigation-link[href='/ru/common/internet']").shouldBe(visible).click();
+            $("div.TabPackageService").should(appear);
+        });
+
+        step("Select Activate Keremet TV", () -> {
+            $("#slide-ftth0371").$("button.btn").click();
+        });
+    }
+
+    @Disabled("Временное отключение Проверки тех возможности")
+    @DisplayName("Проверка технической возможности")
+    @Test
+    void fillTechnicalCheckFormTest() {
+        step("Open main page", () -> {
+            open("/");
+            $("header").should(appear);
+            executeJavaScript("let footer = document.querySelector('footer'); " +
+                    "if (footer) footer.remove();");
+        });
+
+        step("Navigate to Internet section", () -> {
+            $("a.main-menu__navigation-link[href='/ru/common/internet']").shouldBe(visible).click();
+            $("div.TabPackageService").should(appear);
+        });
+
+        step("Navigate to Technical Check section", () -> {
+            $("a.main-menu__submenu-link[href='/ru/technical-check']").shouldBe(visible).click();
+            $("div.technical-check").should(appear);
+        });
+
+        step("Fill phone number", () -> {
+            $("input[name='phone']").shouldBe(visible, enabled).setValue("7000000000");
+        });
+
+    }
+
+}
---
+++ src/test/java/TestBase.java
@@ -0,0 +1,41 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
+
+public class TestBase {
+
+        @BeforeAll
+        static void setup() {
+            Configuration.baseUrl = "https://telecom.kz/ru/";
+            Configuration.pageLoadStrategy = "eager";
+            Configuration.timeout = 10000; // Таймаут в миллисекундах (10 секунд)
+            Configuration.remote = null; //"https://" + System.getProperty("REMOTE_LOGIN") + "@" + System.getProperty("REMOTE_URL");
+            Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+            Configuration.browser = System.getProperty("browser", "chrome");
+            Configuration.browserVersion = System.getProperty("browserVersion", "126.0");
+
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                    "enableVNC", true,
+                    "enableVideo", true,
+                    "videoName", System.getProperty("REMOTE_VIDEO_URL", "default_video.mp4")
+            ));
+            Configuration.browserCapabilities = capabilities;
+
+            SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        }
+
+        @AfterEach
+        void afterEach() {
+            Attach.addVideo();
+            Selenide.closeWebDriver();
+        }
+
+}
